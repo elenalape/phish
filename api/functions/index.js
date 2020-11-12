34a6b8940aefb8fd8c92c19229a067376106617a
elenalape <elenalape@github.com>
2020-11-12 17:12:26 +0000

    add api route for submitting and matching user phishes

--- api/functions/index.js
+++ api/functions/index.js
@@ -133,4 +133,111 @@ app.post("/submit/phish", (req, res) => {
 		});
 });
 
+// ok so if the user doesn't get any matches from the official db
+// they can check it against what others have submitted
+// if it's an unseen thing, add it to the user curated database
+app.post("/submit/userphish", (req, res) => {
+	//create obejct from what person submits
+	const userSubmission = {
+		body: req.body.body,
+		sender: req.body.sender,
+	};
+
+	// first we get all the emails
+	// USER SUBMITTED ONES
+	// get their ids
+	// if similarity score turns out to be quite low, we want to add it to the dabatase
+	db.collection("submittedEmailData")
+		.get()
+		.then((data) => {
+			let emailData = [];
+			data.forEach((doc) => {
+				emailData.push({
+					id: doc.id,
+					emailBody: doc.data().body,
+					exactMatches: doc.data().exactMatches,
+					similarTo: doc.data().similarTo,
+				});
+			});
+
+			let matchesScores = [];
+			let userResponse = {
+				similarToUser: [],
+				exactMatchesToUser: [],
+				scoreToUser: [],
+			};
+
+			emailData.forEach((email) => {
+				const score = stringSimilarity.compareTwoStrings(
+					userSubmission.body,
+					email.emailBody
+				);
+
+				if (score > 0.85) {
+					const newExactMatches = email.exactMatches + 1;
+					const newSimilarTo = email.similarTo + 1;
+					userResponse.similarToUser.push(newSimilarTo);
+					userResponse.exactMatchesToUser.push(newExactMatches);
+					userResponse.scoreToUser.push(score);
+					db.doc(`/emailData/${email.id}`)
+						.get()
+						.then(() => {
+							return db
+								.doc(`/submittedEmailData/${email.id}`)
+								.update({
+									exactMatches: newExactMatches,
+									similarTo: newSimilarTo,
+								});
+						})
+						.catch((err) => {
+							console.error(err);
+							return res.status(500).json({ error: err.code });
+						});
+				} else if (score > 0.4) {
+					const newSimilarTo = email.similarTo + 1;
+					userResponse.similarToUser.push(newSimilarTo);
+					userResponse.exactMatchesToUser.push(email.exactMatches);
+					userResponse.scoreToUser.push(score);
+					db.doc(`/emailData/${email.id}`)
+						.get()
+						.then(() => {
+							return db
+								.doc(`/submittedEmailData/${email.id}`)
+								.update({
+									similarTo: newSimilarTo,
+								});
+						})
+						.catch((err) => {
+							console.error(err);
+							return res.status(500).json({ error: err.code });
+						});
+				} else {
+					db.collection("submittedEmailData")
+						.add({
+							body: userSubmission.body,
+							sender: userSubmission.sender,
+							exactMatches: 1,
+							similarTo: 1,
+						})
+						.then(() => {
+							userResponse.similarToUser.push(0);
+							userResponse.exactMatchesToUser.push(0);
+							userResponse.scoreToUser.push(score);
+						})
+						.catch((err) => {
+							console.error(err);
+						});
+				}
+
+				matchesScores.push(score);
+			});
+
+			return res.json(userResponse);
+		})
+		.catch((err) => {
+			res.status(500).json({ error: "Something went wrong" });
+			console.error(err);
+		});
+});
+
 exports.api = functions.region("europe-west2").https.onRequest(app);
